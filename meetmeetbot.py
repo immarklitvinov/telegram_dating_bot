da960f54c446b2b0d3b0f9bba314a9902038f7ce
vaniilka <mqvp@mail.ru>
2021-08-06 21:10:17 +0300

    Explore & edit left
    I've done everything except explore page, editing of profile and working with database
    yeas!!!

--- meetmeetbot.py
+++ meetmeetbot.py
@@ -1,12 +1,14 @@
 import telebot
 import config
 import emoji
+import random
 from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 bot_me = bot.get_me()
 var_mode = ['main_menu', 0] # selected status
 profile = []
+var_profile_created = False
 
 '''
 
@@ -20,7 +22,9 @@ profile = []
 7 - photo
 '''
 
-basic_interests = ['стройка', 'тик-ток', 'игры', 'шахматы', 'работа', 'тусовки', 'языки', 'рисование', 'бизнес', 'питомцы', 'аниме', 'программирование', 'путешествия', 'общение', 'море', 'музыка', 'фотография', 'концерты', 'торговля', 'автомобили', 'отдых', 'дизайн', 'спорт', 'литература', 'ютуб', 'маркетинг', 'экономика', 'видеоблог', 'танцы', 'стартапы', 'театр', 'дети', '<<continue>>']
+basic_interests = ['стройка', 'тик-ток', 'игры', 'шахматы', 'работа', 'тусовки', 'языки', 'рисование', 'бизнес', 'питомцы', 'аниме', 'программирование', 'путешествия', 'общение', 'море', 'музыка', 'фотография', 'концерты', 'торговля', 'автомобили', 'отдых', 'дизайн', 'спорт', 'литература', 'ютуб', 'маркетинг', 'экономика', 'видеоблог', 'танцы', 'стартапы', 'театр', 'дети']
+random.shuffle(basic_interests)
+basic_interests += ['<<continue>>']
 possible_interests = basic_interests.copy()
 user_interests = []
 
@@ -32,7 +36,7 @@ people = {}
 
 markup_main = types.ReplyKeyboardMarkup(resize_keyboard=True)
 item1_main = types.KeyboardButton("My profile")
-item2_main = types.KeyboardButton("Explore new people")
+item2_main = types.KeyboardButton("Explore")
 markup_main.add(item1_main, item2_main)
 
 markup_back_to_main = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -40,9 +44,16 @@ item1_back_to_main = types.KeyboardButton("Back to main menu")
 markup_back_to_main.add(item1_back_to_main)
 
 markup_profile = types.ReplyKeyboardMarkup(resize_keyboard=True)
-item1_profile = types.KeyboardButton("Create new one")
-item2_profile = types.KeyboardButton("Edit already existing")
-markup_profile.add(item1_profile, item2_profile, item1_back_to_main)
+item1_profile = types.KeyboardButton("Create profile")
+item2_profile = types.KeyboardButton("Edit profile")
+markup_profile.add(item1_profile, item1_back_to_main)
+def markup_profile_update():
+    global markup_profile
+    global item2_profile
+    markup_profile = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup_profile.add(item2_profile, item1_back_to_main)
+
+
 
 markup_sex = types.ReplyKeyboardMarkup(resize_keyboard=True)
 item1_sex = types.KeyboardButton(emoji.emojize('Male :boy:'))
@@ -77,22 +88,29 @@ def photo_download(message):
             new_file.write(downloaded_file)
 
 
+# print existing profile
+def my_profile(chat_id):
+    global people
+    p = people[chat_id]
+    user_caption = f"{p[1]}, {p[4]}, {p[3]}\n\n{p[6]}\n\nInterests: {', '.join(p[5])}"
+    bot.send_photo(chat_id, open(f'images/{"fe" * int(people[chat_id][2] != "M")}male/{people[chat_id][0]}.jpg', 'rb'), caption = user_caption)
+
+
 # handlers
 
 @bot.message_handler(commands=['start'])
 def welcome_command(message):
-    #bot.send_message(message.chat.id, 'Hello! Send /help to get full list of commands')
-    bot.send_message(message.chat.id, f"Welcome, {message.from_user.first_name}!\nI am the bot, created for you to meet new people.\nSend /help to get full list of commands",
-        parse_mode='html', reply_markup = markup_main)
+    bot.send_message(message.chat.id, f"Welcome, {message.from_user.first_name}!\nI am the bot, created for you to meet new people.\nFirst you need to create your profile in <My profile> button, then you will be able to explore others.\nSend /help to get full list of commands", reply_markup = markup_main)
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
-    bot.send_message(message.chat.id, '/start - Begins the dialog\n/help - Shares info about bot\n/profile - Create your profile')
+    bot.send_message(message.from_user.id, '/start - Begins the dialog\n/help - Shares info about bot')
 
 
 @bot.message_handler(content_types=['text'])
 def reply_to_message(message):
     global var_mode
+    global var_profile_created
     global possible_interests
     global user_interests
     global markup_interests
@@ -145,8 +163,14 @@ def reply_to_message(message):
                 bot.send_message(message.chat.id, 'Returning you to main menu', reply_markup = markup_main)
 
         elif message.text == 'My profile':
-            bot.send_message(message.chat.id, 'Choose the required option', reply_markup = markup_profile)
-        elif message.text == 'Create new one':
+            #bot.send_message(message.chat.id, f'{(!var_profile_created) * "Choose the required option" + (var_profile_created) * "Here's your profile"}', reply_markup = markup_profile)
+            if(not var_profile_created):
+                bot.send_message(message.chat.id, "Choose the required option", reply_markup = markup_profile)
+            else:
+                bot.send_message(message.chat.id, "This is your profile now", reply_markup = markup_profile)
+            if(var_profile_created):
+                my_profile(message.from_user.id)
+        elif message.text == 'Create profile' and var_profile_created == False:
             var_mode[0] = 'creating_profile'
             var_mode[1] = 1
             profile.append(message.from_user.id)
@@ -154,6 +178,7 @@ def reply_to_message(message):
         elif message.text == 'Back to main menu':
             var_mode[0] = 'main_menu'
             var_mode[1] = 0
+            profile.clear()
             bot.send_message(message.chat.id, 'Main menu', reply_markup = markup_main)
         else:
             bot.send_message(message.chat.id, 'Message recieved!')
@@ -162,6 +187,7 @@ def reply_to_message(message):
 def reply_to_photo(message):
     global profile
     global people
+    global var_profile_created
     if var_mode[0] == 'creating_profile' and var_mode[1] == 7:
         photo_download(message)
         var_mode[0] = 'main_menu'
@@ -169,7 +195,8 @@ def reply_to_photo(message):
         profile.append(f'/images/{"fe" * int(profile[2] != "M")}male/{message.from_user.id}.jpg')
         people[message.from_user.id] = profile.copy()
         profile.clear()
-        print(people)
+        var_profile_created = True
+        markup_profile_update()
         bot.send_message(message.chat.id, 'The creation of your profile is done. Now you can explore other profiles.', reply_markup = markup_main)
 
 bot.polling()
